7d2bc555b5ea950bbb3ad330f92d93029716d8ad
jamshedirmatov2 <jamshedirmatov2@gmail.com>
2024-03-15 22:27:36 +0500

    Initial commit

---
+++ app/src/main/java/com/wigroup/composeanimation/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.wigroup.composeanimation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.wigroup.composeanimation.ui.theme.ComposeAnimationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeAnimationTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Test()
+                }
+            }
+        }
+    }
+}
