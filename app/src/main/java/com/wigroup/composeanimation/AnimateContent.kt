cec4460a1fa39eac2883be6afdf0a6ed0cd02500
jamshedirmatov2 <jamshedirmatov2@gmail.com>
2024-03-16 14:09:15 +0500

    Animated Content

---
+++ app/src/main/java/com/wigroup/composeanimation/AnimateContent.kt
@@ -0,0 +1,84 @@
+package com.wigroup.composeanimation
+
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AnimateContent() {
+
+    var isFirstScreenLaunched by remember {
+        mutableStateOf(true)
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = { isFirstScreenLaunched = !isFirstScreenLaunched }
+        ) {
+            Text(text = "Switch screens")
+        }
+
+        AnimatedContent(
+            targetState = isFirstScreenLaunched,
+            transitionSpec = {
+                if (targetState) {
+                    slideInHorizontally(tween(2000)) { -it } togetherWith slideOutHorizontally(
+                        tween(2000)
+                    ) { it }
+                } else {
+                    slideInHorizontally(tween(2000)) { it } togetherWith slideOutHorizontally(
+                        tween(2000)
+                    ) { -it }
+                }
+            },
+            label = "Content"
+        ) { shouldLaunchFirstScreen ->
+            if (shouldLaunchFirstScreen) {
+                Screen1()
+            } else {
+                Screen2()
+            }
+        }
+    }
+}
+
+@Composable
+private fun Screen1() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Blue)
+    )
+}
+
+@Composable
+private fun Screen2() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Red)
+    )
+}
